655e5d1dce1cd8e9cb1b6fb87ea4047e6a31b101
Tharindu Nuwan Madhushanka <tharindunm94@gmail.com>
2023-07-30 17:33:49 +0530

    Complete Task 2

--- Demo1.java
+++ Demo1.java
@@ -1,6 +1,6 @@
 public class Demo1 {
     public static void main(String[] args) {
         double bankBalance = 123456.7890;
-        System.out.printf("$ %,.2f \n",bankBalance);
+        System.out.printf("Account Balance = $%,.2f \n",bankBalance);
     }
 }
---
+++ Demo2.java
@@ -0,0 +1,6 @@
+public class Demo2 {
+    public static void main(String[] args) {
+        double valueOfPi = Math.PI;
+        System.out.printf("Value of PI = %.3f\n ",valueOfPi);
+    }
+}
