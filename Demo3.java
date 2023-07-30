cf604340facdce2c140783c009893fea694d858a
Tharindu Nuwan Madhushanka <tharindunm94@gmail.com>
2023-07-30 17:41:57 +0530

    Complete Task 3

---
+++ Demo3.java
@@ -0,0 +1,8 @@
+public class Demo3 {
+    public static void main(String[] args) {
+        int areaCode = 123;
+        int secondPart = 456;
+        int lastPart = 7890;
+        System.out.printf("Telephone Number : \033[34m(%d)\033[0m %d-%d\n ",areaCode,secondPart,lastPart);
+    }
+}
