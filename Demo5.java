7228ad410f3112fe399e09faa1348924bada9ba5
Tharindu Nuwan Madhushanka <tharindunm94@gmail.com>
2023-07-30 18:07:42 +0530

    Complete Task 5

---
+++ Demo5.java
@@ -0,0 +1,10 @@
+public class Demo5 {
+    public static void main(String[] args) {
+        int num1 = 978;
+        int num2 = 3;
+        int num3 = 16;
+        int num4 = 1484100;
+
+        System.out.printf("ISBN Number : \033[32m%d\033[0m-\033[33m%d\033[0m-\033[34m%d\033[0m-\033[35m%d\033[0m\n ",num1,num2,num3,num4);
+    }
+}
